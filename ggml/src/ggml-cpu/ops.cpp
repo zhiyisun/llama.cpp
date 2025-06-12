5a490f07a2aa170c5c7d75fa619d858bbfe718fd
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2025-06-12 23:05:51 +0200

    relax constraints

--- ggml/src/ggml-cpu/ops.cpp
+++ ggml/src/ggml-cpu/ops.cpp
@@ -3208,11 +3208,11 @@ static void ggml_compute_forward_reglu_f32(
     const int ith = params->ith;
     const int nth = params->nth;
 
-    const int nc = dst->ne[0];
+    const int nc = src0->ne[0] / 2;
     const int nr = ggml_nrows(src0);
 
-    GGML_ASSERT(src0->ne[0] / 2 == nc);
-    GGML_ASSERT(ggml_nrows(dst) == nr);
+    GGML_ASSERT(dst->ne[0] >= nc);
+    GGML_ASSERT(ggml_nrows(dst) >= nr);
 
     // rows per thread
     const int dr = (nr + nth - 1)/nth;
@@ -3249,11 +3249,11 @@ static void ggml_compute_forward_reglu_f16(
     const int ith = params->ith;
     const int nth = params->nth;
 
-    const int nc = dst->ne[0];
+    const int nc = src0->ne[0] / 2;
     const int nr = ggml_nrows(src0);
 
-    GGML_ASSERT(src0->ne[0] / 2 == nc);
-    GGML_ASSERT(ggml_nrows(dst) == nr);
+    GGML_ASSERT(dst->ne[0] >= nc);
+    GGML_ASSERT(ggml_nrows(dst) >= nr);
 
     // rows per thread
     const int dr = (nr + nth - 1)/nth;
@@ -3315,11 +3315,11 @@ static void ggml_compute_forward_geglu_f32(
     const int ith = params->ith;
     const int nth = params->nth;
 
-    const int nc = dst->ne[0];
+    const int nc = src0->ne[0] / 2;
     const int nr = ggml_nrows(src0);
 
-    GGML_ASSERT(src0->ne[0] / 2 == nc);
-    GGML_ASSERT(ggml_nrows(dst) == nr);
+    GGML_ASSERT(dst->ne[0] >= nc);
+    GGML_ASSERT(ggml_nrows(dst) >= nr);
 
     // rows per thread
     const int dr = (nr + nth - 1)/nth;
@@ -3356,11 +3356,11 @@ static void ggml_compute_forward_geglu_f16(
     const int ith = params->ith;
     const int nth = params->nth;
 
-    const int nc = dst->ne[0];
+    const int nc = src0->ne[0] / 2;
     const int nr = ggml_nrows(src0);
 
-    GGML_ASSERT(src0->ne[0] / 2 == nc);
-    GGML_ASSERT(ggml_nrows(dst) == nr);
+    GGML_ASSERT(dst->ne[0] >= nc);
+    GGML_ASSERT(ggml_nrows(dst) >= nr);
 
     // rows per thread
     const int dr = (nr + nth - 1)/nth;
@@ -3422,11 +3422,11 @@ static void ggml_compute_forward_swiglu_f32(
     const int ith = params->ith;
     const int nth = params->nth;
 
-    const int nc = dst->ne[0];
+    const int nc = src0->ne[0] / 2;
     const int nr = ggml_nrows(src0);
 
-    GGML_ASSERT(src0->ne[0] / 2 == nc);
-    GGML_ASSERT(ggml_nrows(dst) == nr);
+    GGML_ASSERT(dst->ne[0] >= nc);
+    GGML_ASSERT(ggml_nrows(dst) >= nr);
 
     // rows per thread
     const int dr = (nr + nth - 1)/nth;
@@ -3463,11 +3463,11 @@ static void ggml_compute_forward_swiglu_f16(
     const int ith = params->ith;
     const int nth = params->nth;
 
-    const int nc = dst->ne[0];
+    const int nc = src0->ne[0] / 2;
     const int nr = ggml_nrows(src0);
 
-    GGML_ASSERT(src0->ne[0] / 2 == nc);
-    GGML_ASSERT(ggml_nrows(dst) == nr);
+    GGML_ASSERT(dst->ne[0] >= nc);
+    GGML_ASSERT(ggml_nrows(dst) >= nr);
 
     // rows per thread
     const int dr = (nr + nth - 1)/nth;
